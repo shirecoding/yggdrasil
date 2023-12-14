fe3f7b1de9ae77b8e143e3f16c41c81412a5dbcb
benjaminhon <benjaminhon86@gmail.com>
2023-12-14 17:43:50 +0800

    Add messagebox

--- yggdrasil-bolt/app/src/components/message-box.ts
+++ yggdrasil-bolt/app/src/components/message-box.ts
@@ -1,25 +1,34 @@
 import { LitElement, html, css, PropertyValueMap } from "lit";
 import { customElement, property, state, query } from "lit/decorators.js";
 
+export interface Message {
+  body: string;
+  author: string;
+}
+
 @customElement("message-box")
 export class MessageBox extends LitElement {
   @query("#message-list")
   accessor messageList!: HTMLElement;
 
+  @query("#message-form")
+  accessor messageForm: any;
+
   static styles = css`
     .message-list {
       overflow-y: auto;
+      min-height: 200px;
       max-height: 300px; /* Set a maximum height for the chat window, adjust as needed */
       border: 1px solid #ccc; /* Add styling as needed */
     }
   `;
 
-  addMessage(message) {
+  addMessage(message: Message) {
     const item = document.createElement("chat-message");
     item.innerHTML = message.body;
     item.setAttribute(
       "msgType",
-      message.author === "admin" ? "msg-receive" : "msg-send"
+      message.author === "system" ? "msg-receive" : "msg-send"
     );
     // Add it to the light DOM
     this.appendChild(item);
@@ -30,9 +39,30 @@ export class MessageBox extends LitElement {
   }
 
   protected firstUpdated() {
+    // Scroll to bottom on mutation
     this.messageList.addEventListener("sl-mutation", (event) => {
-      console.log(event);
+      console.log("scroll to bottom");
       this.scrollToBottom();
+    });
+
+    this.messageForm.addEventListener("submit", (e: FormDataEvent) => {
+      e.preventDefault(); // prevent refresh browser
+      new FormData(this.messageForm); // construct a FormData object, which fires the formdata event
+    });
+
+    this.messageForm.addEventListener("formdata", (e: FormDataEvent) => {
+      const parsed = Object.fromEntries(Array.from(e.formData.entries()));
+      // Dispatch on-create event
+      this.dispatchEvent(
+        new CustomEvent<Message>("on-message", {
+          bubbles: true,
+          composed: true,
+          detail: {
+            body: parsed.message.toString(),
+            author: "",
+          },
+        })
+      );
     });
   }
 
@@ -43,6 +73,16 @@ export class MessageBox extends LitElement {
           <slot></slot>
         </sl-mutation-observer>
       </div>
+      <br />
+      <form action="submit" id="message-form">
+        <sl-input name="message" size="large">
+          <sl-icon-button
+            type="submit"
+            name="chat"
+            slot="suffix"
+          ></sl-icon-button>
+        </sl-input>
+      </form>
     `;
   }
 }
--- yggdrasil-bolt/app/src/pages/game-page.ts
+++ yggdrasil-bolt/app/src/pages/game-page.ts
@@ -1,18 +1,31 @@
 import { LitElement, html, css } from "lit";
-import { customElement, property, state } from "lit/decorators.js";
+import { customElement, property, state, query } from "lit/decorators.js";
+import { Message, MessageBox } from "../components/message-box";
 
 @customElement("game-page")
 export class GamePage extends LitElement {
+  @query("#message-box")
+  accessor messageBox!: MessageBox;
+
+  onMessage(e: CustomEvent<Message>) {
+    const { author, body } = e.detail;
+    console.log(body, author);
+    this.messageBox.addMessage({
+      body: body,
+      author: author,
+    });
+  }
+
   render() {
     return html`
       <h1>Game Page</h1>
 
-      <message-box>
-        <p>hello</p>
-        <p>world</p>
-      </message-box>
+      <message-box
+        @on-message="${this.onMessage}"
+        id="message-box"
+      ></message-box>
 
-      <sl-menu style="max-width: 200px;">
+      <!-- <sl-menu style="max-width: 200px;">
         <sl-menu-label>Fruits</sl-menu-label>
         <sl-menu-item value="apple">Apple</sl-menu-item>
         <sl-menu-item value="banana">Banana</sl-menu-item>
@@ -22,9 +35,7 @@ export class GamePage extends LitElement {
         <sl-menu-item value="broccoli">Broccoli</sl-menu-item>
         <sl-menu-item value="carrot">Carrot</sl-menu-item>
         <sl-menu-item value="zucchini">Zucchini</sl-menu-item>
-      </sl-menu>
-
-      <sl-input></sl-input>
+      </sl-menu> -->
     `;
   }
 }
